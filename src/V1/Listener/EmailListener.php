34ec8dfa24e4f3b8ca6a2f84ec91d7961ba31708
visa4 <antonino.visalli@gmail.com>
2016-12-06 11:13:21 +0100

    Add email listener

---
+++ src/V1/Listener/EmailListener.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Strapieno\UserCheckIdentity\Api\V1\Listener;
+
+use MailMan\Message;
+use Matryoshka\Model\Object\ActiveRecord\ActiveRecordInterface;
+use Strapieno\User\Model\Entity\UserInterface;
+use Strapieno\Utils\Model\Entity\IdentityExistAwareInterface;
+use Zend\EventManager\Event;
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
+use Zend\EventManager\ListenerAggregateTrait;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorAwareTrait;
+use ZF\Hal\Entity;
+
+/**
+ * Class EmailListener
+ */
+class EmailListener implements ListenerAggregateInterface, ServiceLocatorAwareInterface
+{
+    use ListenerAggregateTrait;
+    use ServiceLocatorAwareTrait;
+
+    /**
+     * @var string
+     */
+    protected $subject;
+
+    /**
+     * @var string
+     */
+    protected $template;
+
+    /**
+     *  {@inheritdoc}
+     */
+    public function attach(EventManagerInterface $events)
+    {
+        $this->listeners[] = $events->attach('create.post', [$this, 'sendValidationMail']);
+    }
+
+    /**
+     * @param Event $e
+     */
+    public function sendValidationMail(Event $e)
+    {
+        $hal = $e->getParam('entity');
+        if ($hal instanceof Entity
+            && ($user = $hal->entity)
+            && $user instanceof IdentityExistAwareInterface
+            && $user instanceof ActiveRecordInterface
+            && $user instanceof UserInterface)
+        {
+            $message = $this->getMessage($user);
+            $this->getServiceLocator()->get('MailMan\Service\MailInterface')->send($message);
+        }
+    }
+
+    /**
+     * @param IdentityExistAwareInterface $user
+     * @return ViewModel
+     */
+    protected function getTemplateMail(IdentityExistAwareInterface $user)
+    {
+        $template = new ViewModel();
+        $template->setVariable('user', $user);
+        $template->setTemplate($this->getTemplateMail());
+
+        return $template;
+    }
+
+    /**
+     * @param UserInterface $user
+     * @return Message
+     */
+    protected function getMessage(UserInterface $user)
+    {
+        $viewRenderer = $this->getServiceLocator()->get('ViewRenderer');
+
+        $message = new Message();
+        $message->setSubject($this->getSubject());
+        $message->setTo($user->getEmail());
+        $message->addHtmlPart($viewRenderer->render($this->getTemplateMail($user)));
+
+        return $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param string $subject
+     * @return $this
+     */
+    public function setSubject(mixed $subject)
+    {
+        $this->subject = $subject;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * @param string $template
+     * @return $this
+     */
+    public function setTemplate(mixed $template)
+    {
+        $this->template = $template;
+        return $this;
+    }
+}
---
+++ src/V1/Listener/EmailListenerFactory.php
@@ -0,0 +1,30 @@
+<?php
+namespace Strapieno\UserCheckIdentity\Api\V1\Listener;
+
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class EmailListenerFactory implements FactoryInterface
+{
+    /**
+     * @param ServiceLocatorInterface $serviceLocator
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $services->get('Config');
+
+        if (!isset($config['user_listener']['template'])) {
+           throw new ServiceNotCreatedException('Template not set in EmailListenerFactory');
+        }
+
+        if (!isset($config['user_listener']['subject'])) {
+            throw new ServiceNotCreatedException('Subject not set in EmailListenerFactory');
+        }
+
+        $listener = new EmailListener();
+        $listener->setSubject($config['user_listener']['subject']);
+        $listener->setTemplate($config['user_listener']['template']);
+        return $listener;
+    }
+}
